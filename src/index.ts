c46bf0667373da71d847165de65ffbada99b2737
Nate Schultz <nate.schultz@outlook.com>
2026-06-03 00:25:55 -0400

    fix

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,8 @@ const getAllowedOrigins = (): string[] => {
       'http://localhost:3002',
       'http://localhost:3003',
       'http://localhost:3004',
-      'http://localhost:3005'
+      'http://localhost:3005',
+      'http://localhost:5173'
     ];
   } else if (process.env.NODE_ENV === 'staging') {
     const stagingOrigins = [
